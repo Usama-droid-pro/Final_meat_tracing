3951accda43941623a21d68ef9fc37d1c51dd2dd
Usama <100606335+Usama-droid-pro@users.noreply.github.com>
2023-03-17 14:06:14 +0500

    Update index.js

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ const app= express();
 const PORT = 3000;
 
 // const userLogsModel= require('./models/userLogsModels')
-app.use("/animal_images" , express.static('/animal_images'))
+app.use("/tmp" , express.static('/tmp'))
 
 const cors = require('cors');
 mongoose.set('strictQuery' , false)
 
-app.use("/animal_images" , express.static("animal_images"))
+app.use("/tmp" , express.static("tmp"))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(cors({
